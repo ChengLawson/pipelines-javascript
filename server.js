695384e6b82def77bd035bd7294dfd3f7546bcf2
ChengLawson <cheng.lawson@uk.ibm.com>
2019-04-25 15:34:08 +0100

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Azure DevOps Node.js Webapp\nApplication variable: ' + process.env.APP_VARIABLE);
+  res.send('Azure DevOps Node.js Webapp. Application variable: ' + process.env.APP_VARIABLE);
 });
 
 var port = process.env.PORT||PORT;
