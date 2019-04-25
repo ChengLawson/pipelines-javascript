ed863b3a9d7f2ebe0c87f96f1b64f6d47111fb98
ChengLawson <cheng.lawson@uk.ibm.com>
2019-04-25 11:47:56 +0100

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Azure DevOps Node.js Sample!\n');
+  res.send('Azure DevOps Pipelines Node.js Sample!\n');
 });
 
 var port = process.env.PORT||PORT;
